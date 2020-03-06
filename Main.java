9f939e01890ffb3ceae6543adef28acc3111f320
Yuliya Frolova <jupunkevich@gmail.com>
2020-03-06 07:39:43 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentBallance = 2_000_000_000;
+        int transaction = 500_000_000;
+        int total = currentBallance + transaction;
+        System.out.println(total);
+    }
+}
